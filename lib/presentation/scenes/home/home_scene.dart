f387747fbe29ad5441dd87846f162b11ae3b07be
aRobihamanto <robi.hamanto@access-company.com>
2021-06-03 14:55:56 +0800

    add topbanner section

--- lib/presentation/scenes/home/home_scene.dart
+++ lib/presentation/scenes/home/home_scene.dart
@@ -1,4 +1,7 @@
+import 'package:Raise_Flutter/constants.dart';
+import 'package:Raise_Flutter/presentation/widgets/cache_image_network.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'home_bloc.dart';
 
@@ -12,17 +15,41 @@ class HomeScene extends StatefulWidget {
 }
 
 class _HomeSceneState extends State<HomeScene> {
-  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
-
+    final banners = widget.bloc.topBanners.banners;
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-
-        ],
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+            height: 164,
+            width: double.infinity,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: banners.length,
+              itemBuilder: (context, index) {
+                final banner = banners[index];
+                return Container(
+                  height: 164,
+                  width: 308,
+                  child: Padding(
+                      padding: EdgeInsets.only(left: kDefaultPadding / 2),
+                      child: CacheImageNetwork(banner.url)
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            flex: 10,
+              child: Container(
+                color: Colors.black,
+            )
+          )
+        ]
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
