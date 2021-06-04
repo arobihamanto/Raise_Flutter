7a7b2bb7aac8f947698e26c800f7219e4ce9e947
aRobihamanto <robi.hamanto@access-company.com>
2021-06-04 13:02:49 +0800

    add ranking section

---
+++ lib/presentation/scenes/home/ranking_section.dart
@@ -0,0 +1,124 @@
+import 'package:Raise_Flutter/constants.dart';
+import 'package:Raise_Flutter/domain/entities/home_model.dart';
+import 'package:Raise_Flutter/presentation/widgets/cache_image_network.dart';
+import 'package:Raise_Flutter/utils/size_config.dart';
+import 'package:flutter/material.dart';
+
+class RankingSection extends StatelessWidget {
+  final Section section;
+
+  RankingSection({this.section});
+
+  @override
+  Widget build(BuildContext context) {
+    final ranking = section.rankings.first;
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: kDefaultPadding / 2),
+            child: Row(
+              children: [
+                Text(section.title),
+                Spacer(),
+                Text('>'),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Opacity(
+                  opacity: 0.4,
+                  child: Container(
+                      width: SizeConfig.screenWidth / 3,
+                      child: RankingSectionItem(ranking: ranking)
+                  ),
+                ),
+                Container(
+                    width: SizeConfig.screenWidth / 3,
+                    child: RankingSectionItem(ranking: ranking)
+                ),
+                Opacity(
+                  opacity: 0.4,
+                  child: Container(
+                      width: SizeConfig.screenWidth / 3,
+                      child: RankingSectionItem(ranking: ranking)
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class RankingSectionItem extends StatelessWidget {
+
+  final Ranking ranking;
+
+  RankingSectionItem({this.ranking});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.symmetric(
+          horizontal: kDefaultPadding / 2, vertical: kDefaultPadding / 2),
+      scrollDirection: Axis.vertical,
+      children: [
+        ...ranking.works.map((e) => Container(
+            padding: EdgeInsets.only(bottom: kDefaultPadding / 2),
+            height: SizeConfig.screenHeight / 10,
+            child: RankingWorkItem(work: e)))
+      ],
+    );
+  }
+}
+
+
+class RankingWorkItem extends StatelessWidget {
+  final RankingWork work;
+
+  RankingWorkItem({this.work});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Expanded(
+            child: CacheImageNetwork(
+              imageURL: work.image.url,
+            ),
+          ),
+          Expanded(
+              flex: 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: kDefaultPadding),
+                      Expanded(child: Text(work.title, maxLines: 1,)),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(width: kDefaultPadding),
+                      Expanded(child: Text('👁  ${work.viewCount}', maxLines: 1, style: TextStyle(fontSize: 10),)),
+                      SizedBox(width: kDefaultPadding / 2),
+                      Expanded(child: Text('🏳️  ${work.commentCount}', maxLines: 1, style: TextStyle(fontSize: 10),)),
+                    ],
+                  ),
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
