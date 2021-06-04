3b7eee9faada4756e605c7bfae091f06e8726de4
aRobihamanto <robi.hamanto@access-company.com>
2021-06-04 13:03:08 +0800

    update home scene

--- lib/presentation/scenes/home/home_scene.dart
+++ lib/presentation/scenes/home/home_scene.dart
@@ -1,6 +1,8 @@
 import 'package:Raise_Flutter/constants.dart';
 import 'package:Raise_Flutter/domain/entities/home_model.dart';
 import 'package:Raise_Flutter/presentation/scenes/home/daily_ranking_section.dart';
+import 'package:Raise_Flutter/presentation/scenes/home/free_only_now_section.dart';
+import 'package:Raise_Flutter/presentation/scenes/home/ranking_section.dart';
 import 'package:Raise_Flutter/presentation/scenes/home/top_banner_section.dart';
 import 'package:Raise_Flutter/presentation/widgets/cache_image_network.dart';
 import 'package:Raise_Flutter/utils/size_config.dart';
@@ -20,15 +22,15 @@ class _HomeSceneState extends State<HomeScene> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-              //mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 TopBannerSection(section: widget.bloc.topBanners),
                 DailyRankingSection(section: widget.bloc.dailyRanking),
+                FreeOnlyNowSection(section: widget.bloc.freOnlyNow),
+                RankingSection(section: widget.bloc.ranking),
               ]),
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
